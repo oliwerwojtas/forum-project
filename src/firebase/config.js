a663f855ae70f27bc163e3556b8349411967200b
Titter00 <olo_wojtas@o2.pl>
2023-02-12 10:09:55 +0100

    feat: :art: create connection  and authentication with firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRbeqa5Yp3NoWrdFZ2plOsSrrOpuCeseQ",
+  authDomain: "managment-app-5c446.firebaseapp.com",
+  projectId: "managment-app-5c446",
+  storageBucket: "managment-app-5c446.appspot.com",
+  messagingSenderId: "198722011929",
+  appId: "1:198722011929:web:c03c3462c73835d846bb88",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//for time in the future
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
