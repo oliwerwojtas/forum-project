8b2164ef597b196f1da2ab4785591e6cc3563de5
Titter00 <olo_wojtas@o2.pl>
2023-02-12 15:46:13 +0100

    feat: :sparkles: implement page setup using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Create from "./pages/Create";
+import Details from "./pages/Details";
+import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "/create", element: <Create /> },
+      { path: "/details/:id", element: <Details /> },
+    ],
+  },
+]);
 
 function App() {
-  return <p>Hello</p>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
