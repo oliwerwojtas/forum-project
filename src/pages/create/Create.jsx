c202b6a90ae87a2a29996dceea3e9c094f515b9c
Titter00 <olo_wojtas@o2.pl>
2023-02-23 19:46:57 +0100

    feat: :art: use React Select package to control lists insted of basic select option

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,14 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Select from "react-select";
 import "./Create.css";
+import { useCollection } from "../../hooks/useCollection";
+
+const categories = [
+  { value: "hobby", label: "Hobby" },
+  { value: "fashion", label: "Fashion" },
+  { value: "traveling", label: "Traveling" },
+  { value: "work", label: "Work" },
+];
 const Create = () => {
+  const { documents } = useCollection("users");
+  console.log(documents);
+  const [users, setUsers] = useState([]);
   const [name, setName] = useState("");
   const [details, setDetails] = useState("");
   const [date, setDate] = useState("");
   const [category, setCategory] = useState("");
   const [assignedUsers, setAssignedUsers] = useState([]);
+  const [checkError, setCheckError] = useState(null);
+
+  useEffect(() => {
+    if (documents) {
+      const options = documents.map((user) => {
+        return { value: user, label: user.displayName };
+      });
+      setUsers(options);
+    }
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name, details, date);
+    setCheckError(null);
+    console.log(name, details, date, category.value, assignedUsers);
   };
   return (
     <div className="form">
@@ -43,9 +67,15 @@ const Create = () => {
         </label>
         <label>
           <span>Category:</span>
-          <span>Assigned to:</span>
+          <Select onChange={(option) => setCategory(option)} options={categories} />
+        </label>
+        <label>
+          <span>Assign to:</span>
+          <Select onChange={(option) => setAssignedUsers(option)} options={users} isMulti />
         </label>
         <button className="btn">Add Subject</button>
+
+        {checkError && <p className="error">{checkError}</p>}
       </form>
     </div>
   );
